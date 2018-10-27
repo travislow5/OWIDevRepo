f2862327e00e366d259783a30f02611e0f4382fa
BajazzoGiggles <36993721+BajazzoGiggles@users.noreply.github.com>
2018-10-27 15:33:27 +0200

    api appendage

--- movies.js
+++ movies.js
@@ -5,30 +5,32 @@ $(function(){
     
     var apiRequest = new XMLHttpRequest();
     
-    var variableName = "Girls";
-    
-    apiRequest.open('GET', 'http://www.omdbapi.com/?apikey=26f0eb1&s="' + variableName + '"', true);
+    apiRequest.open('GET', 'http://www.omdbapi.com/?apikey=26f0eb1&s="Mean Girls"', true);
                     
     
     
         apiRequest.onload = function (){
         
-        var data = JSON.parse(this.response);     
+        var data = JSON.parse(this.response);   
+            
+            console.log(apiRequest);
 
         function createContainers(movieTitle, moviePoster, genre){
             
-            $("#movieInformation").append("<div class='movie-information-container col-xs-3'><div class='individualMovie'><img src=" + moviePoster +"><h4 id='movie-name'> " + movieTitle + "</h4><h6 id='movie-genre'> " + genre + "</h6><button><img src='../img/movieinfo.png' href='individual_movie.html'></button></div>");            
-           
+            $(".movieInformation").append("<div class='movie-information-container col-xs-3 content-center'><div class='individualMovie'><img src=" + moviePoster +"></div><div class='movieInfo'><h4 id='movie-name'>" + movieTitle + "</h4><h6 id='movie-genre'>" + genre + "</h6></div><button><img src='../img/movieinfo.png' href='individual_movie.html'></button></div>");            
+ 
+           }
+            
+                       
             for(i=0; i < data.Search.length; i++){
                
                movieTitle = data.Search[i].Title;
                moviePoster = data.Search[i].Poster;
-               genre = data.Search[i].genre;
+               genre = data.Search[i].Genre;
                
                createContainers(movieTitle, moviePoster, genre);
                 
-              
-           } 
+             
       
         }
               
