9fe699f363346eead59abaa9747a134e482bfa2d
Lee-Anne <180085@virtualwindow>
2018-10-29 12:08:42 +0200

    Watch List Update

--- js/watchlist.js
+++ js/watchlist.js
@@ -2,8 +2,8 @@ $(function(){
   
     console.log("hi");
     
-    function CreateCards(MovieTitle, MovieImage){
-        $("#Watchlist").append("<div class='Info card'><div class='card-header' id='Title-One'><h5 class='mb-0'><p class='Movie-Title'>" + MovieTitle + "</p><button class='Chev-button btn btn-link' type='button' data-toggle='collapse' data-target='#InfoOne'><img class='Chevron' src='../img/chevron.png'></button></h5></div><!--card header--><div id='InfoOne' class='collapse show' data-parent='#Watchlist'><div class='card-body'><div class='row'><div class='MovieCard col-6'><img src='" + MovieImage + "' alt='Movie image'></div><!--MovieCard--><div class='col-6 box-parent'><div class='buttons box-child'><a href='#' class='button btn btn-default'>Watch Now</a><a href='#' class='remove button btn btn-default'>Remove</a></div><!--buttons--></div></div><!--row--></div><!--card-body--></div><!--InfoOne--></div><!--card-->")
+    function CreateCards(MovieTitle, MovieImage, MovieCounter){
+        $("#Watchlist").append("<div class='Info card'><div class='card-header' id='Title-One'><h5 class='mb-0'><p class='Movie-Title'>" + MovieTitle + "</p><button class='Chev-button btn btn-link' type='button' data-toggle='collapse' data-target='#Info" + MovieCounter + "'><img class='Chevron' src='../img/chevron.png'></button></h5></div><!--card header--><div id='Info"+ MovieCounter + "' class='collapse show' data-parent='#Watchlist'><div class='card-body'><div class='row'><div class='MovieCard col-6'><img src='" + MovieImage + "' alt='Movie image'></div><!--MovieCard--><div class='col-6 box-parent'><div class='buttons box-child'><a href='#' class='button btn btn-default'>Watch Now</a><a href='#' class='remove button btn btn-default'>Remove</a></div><!--buttons--></div></div><!--row--></div><!--card-body--></div><!--InfoOne--></div><!--card-->")
     };
   
     
@@ -23,12 +23,16 @@ $(function(){
     
   */
 // var watchlistArray = [{title: "Venom", ..... poster:"http//..."}] 
-
-   /* for(i=0, i < 10; i++){
-        var movieName = Walle;
-        var movieImage = "../imgs/walle.jpg";
-        
-        CreateCards(movieName, movieImage);
-    }    */
+    
+    var movieCounter = 3
+    
+    for(i=0; i < 1; i++){
+        var movieName = "Walle";
+        var movieImage = "../img/bladerunner.jpg";
+        var movieCounter = movieCounter + 1;
+        CreateCards(movieName, movieImage, movieCounter);
+    } ;  
+    
     
     });
+
