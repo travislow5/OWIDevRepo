0faddad3cc0decb8cd1ee938ca1b11529269d382
HeleneWentworth <180022@virtualwindow.co.za>
2018-10-30 00:51:45 +0200

    JavaScript
    Its not working I cant get it to work

---
+++ js/login.js
@@ -0,0 +1,14 @@
+ function validate() {
+            var email = document.getElementById("Email").value;
+            var password = document.getElementById("Password").value;
+            if ( email == null || email == "") {
+                alert("Please enter your email.");
+                return false;
+            }
+            if (password == null || password == "") {
+                alert("Please enter the password.");
+                return false;
+            }
+            alert('Login successful');
+            
+        } 
