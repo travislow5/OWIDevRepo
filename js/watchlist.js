c2c1efa1fa4a4a4e0e430f1db88a90f2d696e081
Lee-Anne <180085@virtualwindow>
2018-10-29 11:01:47 +0200

    Watch List Remove Button Update

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,7 +1,9 @@
 $(function(){
   
+    console.log("hi");
+    
     function CreateCards(MovieTitle, MovieImage){
-        $("#Watchlist").append("<div class='Info card'><div class='card-header' id='Title-One'><h5 class='mb-0'><p class='Movie-Title'>" + MovieTitle + "</p><button class='Chev-button btn btn-link' type='button' data-toggle='collapse' data-target='#InfoOne'><img class='Chevron' src='../img/chevron.png'></button></h5></div><!--card header--><div id='InfoOne' class='collapse show' data-parent='#Watchlist'><div class='card-body'><div class='row'><div class='MovieCard col-6'><img src='" + MovieImage + "' alt='Movie image'></div><!--MovieCard--><div class='col-6 box-parent'><div class='buttons box-child'><a href='#' class='button btn btn-default'>Watch Now</a><a href='#' class='remove button btn btn-default'>Remove</a></div><!--buttons--></div></div><!--row--></div><!--card-body--></div><!--InfoOne--></div><!--card-->");
+        $("#Watchlist").append("<div class='Info card'><div class='card-header' id='Title-One'><h5 class='mb-0'><p class='Movie-Title'>" + MovieTitle + "</p><button class='Chev-button btn btn-link' type='button' data-toggle='collapse' data-target='#InfoOne'><img class='Chevron' src='../img/chevron.png'></button></h5></div><!--card header--><div id='InfoOne' class='collapse show' data-parent='#Watchlist'><div class='card-body'><div class='row'><div class='MovieCard col-6'><img src='" + MovieImage + "' alt='Movie image'></div><!--MovieCard--><div class='col-6 box-parent'><div class='buttons box-child'><a href='#' class='button btn btn-default'>Watch Now</a><a href='#' class='remove button btn btn-default'>Remove</a></div><!--buttons--></div></div><!--row--></div><!--card-body--></div><!--InfoOne--></div><!--card-->")
     };
   
     
