24a922a191fda6d572a1991ca84d879b6a7771b5
BajazzoGiggles <36993721+BajazzoGiggles@users.noreply.github.com>
2018-10-30 22:07:48 +0200

    Updated movies.js

--- js/movies.js
+++ js/movies.js
@@ -1,91 +1,95 @@
-//Navbar function:
 $(function(){
     
+    var randomMovieArray = ['American Pie', 'Lilo & Stitch', 'Lord of the Rings', 'Harry Potter', 'Moana', 'Lion King', 'Coraline', 'Beetlejuice', 'Jurassic Park', 'Beauty and the Beast', 'All Good Things', 'Deadpool', 'Pretty Woman', 'Dirty Dancing', 'Firewall'];
+
+function apiCall(){
+    
+    
+    for(i = 0; i < randomMovieArray.length; i++){
+        
+        var randomMovie = randomMovieArray[i];
+        console.log(randomMovie);
+        
+        
+        $.getJSON('http://www.omdbapi.com/?apikey=26f0eb1&t=' + encodeURI(randomMovie)).then(function(response){
+        var movieTitle = response.Title;
+        var movieYear = response.Year;
+        var movieGenre = response.Genre;
+        var moviePoster = response.Poster;
+        var movieDirector = response.Director;
+        var movieRuntime = response.Runtime;
+        var moviePlot = response.Plot;
+        var movieActors = response.Actors;
+
+        console.log(response.Actors);
+        
+        
+         $(".movieInformation").append("<div id='movie-information-container' class='col-xs-12 col-sm-12 col-md-4 col-lg-3'><div class='movie-holder'><img id='movie_poster' src=" + moviePoster + "><h3 id='movie_title'>Title:" + movieTitle + "</h3><h4 id='move_year'>Year:" + movieYear + "</h4><h4 id='movie_genre'>Genre:" + movieGenre + "</h4><a href='individual_movie.html' ><button class='addMovie'>Movie Info</button></a><p id='runTime'>" + movieRuntime + "</p><p id='movie_director'>" + movieDirector + "</p><p id='movie_plot'>" + moviePlot + "</p><p id='movie_actors'>" + movieActors + "</p></div><br></div>");
+            
+      
+        });
+        
+        
+        
+    }//end of for 
+    
+    
+    $(".movieInformation").on("click", "button", function(){
+      
+        var runTimeOfMovie = $(this).siblings("#runTime").text();
+        var titleOfMovie = $(this).siblings("#movie_title").text();
+        var yearOfMovie = $(this).siblings("#move_year").text();
+        var genreOfMovie = $(this).siblings("#movie_genre").text();
+        var directorOfMovie = $(this).siblings("#movie_director").text();
+        var plotOfMovie = $(this).siblings("#movie_plot").text();
+        var actorsOfMovie = $(this).siblings("#movie_actors").text();
+        
+
+        console.log(titleOfMovie);
+        
+        for(i=0; i > randomMovieArray.length; i ++){
+            
+            if(randomMovieArray[i] === titleOfMovie){
+                
+                var title = response[i].Title;
+                var director = response[i].Director;
+                var actors = response[i].Actors;
+                var genre = response[i].Genre;
+                var runTime = response[i].Runtime;
+                        
+                localStorage.setItem("title", titleOfMovie);
+                localStorage.setItem("director", directorOfMovie);
+                localStorage.setItem("actors", actorsOfMovie);
+                localStorage.setItem("genre", genreOfMovie);
+                localStorage.setItem("runTime", runTimeOfMovie);
+            }
+        
+        }
+
+        
+        
+    });
+    
+}// end of function 
+    
+  
+       
+                
    
     
-    var apiRequest = new XMLHttpRequest();
     
-    apiRequest.open('GET', 'http://www.omdbapi.com/?apikey=26f0eb1&s="Guardians of the Galaxy"&y=2014&i=tt2015381', true);
-                    
         
-    
-        apiRequest.onload = function (){
-        
-        var data = JSON.parse(this.response);
         
 
-            console.log(apiRequest);
 
-        function createContainers(movieTitle, moviePoster, year){
-            
-            $(".movieInformation").append("<div class='movie-information-container col-xs-3 content-center col-xs-pull-1 col-sm-pull-0 col-md-pull-0 row'><div class='individualMovie'><img class='img-fluid' src=" + moviePoster +"></div><div class='movieInfo'><h4 id='movie-name'>" + movieTitle + "</h4><h6 id='movie-year'>" + year + "</h6></div><button id='movie-btn' class='btn btn-space img-fluid' href='individual_movie.html'><img src='../img/movieinfo.png' href='individual_movie.html'></button></div>");            
- 
-           }
-            
-                       
-            for(i=0; i < data.Search.length; i++){
-               
-               movieTitle = data.Search[i].Title;
-               moviePoster = data.Search[i].Poster;
-               year = data.Search[i].Year;
-               
-               createContainers(movieTitle, moviePoster, year);
-                
-        }
-            
-            $(".movie-information-container").on("click", function(){
-                
-                var storageReference = +$(this).find("#movie-year").text();
-                console.log(storageReference);
-                
-                for(i=0; i < data.Search.length; i++){
-                    
-                    if(data.Search[i].year === storageReference){
-                        
-                        var title = data.Search[i].Title;
-                        var director = data.Search[i].Director;
-                        var actors = data.Search[i].Actors;
-                        var genre = data.Search[i].Genre;
-                        var runTime = data.Search[i].Runtime;
-                        
-                        localStorage.setItem("title", title);
-                        localStorage.setItem("director", director);
-                        localStorage.setItem("actors", actors);
-                        localStorage.setItem("genre", genre);
-                        localStorage.setItem("runTime", runTime);
-                    }
-                }
-            });
-              
-        
-    }
+apiCall();
     
-         
-     
-    apiRequest.send();
-
-            
- 
-    
-    
-
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
-    });  
-
-
-//Display Movies function
-
-
+    }); 
     
+     
     
-    
-   
-    
+});
 
-    
-        
-
-    
-})
