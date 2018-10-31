85f85e076e54e7e53c12cea6aa7f30f93dde7324
Mitchell Claasen <180039@virtualwindow.co.za>
2018-10-31 22:27:33 +0200

    Home page finalised

--- js/login.js
+++ js/login.js
@@ -1,14 +1,15 @@
  function validate() {
-            var email = document.getElementById("Email").value;
-            var password = document.getElementById("Password").value;
-            if ( email == null || email == "") {
-                alert("Please enter your email.");
-                return false;
-            }
-            if (password == null || password == "") {
-                alert("Please enter the password.");
-                return false;
-            }
-            alert('Login successful');
-            
-        } 
+     var email = document.getElementById("Email").value;
+     var password = document.getElementById("Password").value;
+     if (email == null || email == "") {
+         alert("Please enter your email.");
+         return false;
+     } else {
+         if (password == null || password == "") {
+             alert("Please enter the password.");
+             return false;
+         }else{
+                  alert('Login successful');
+         }
+     }
+ }
